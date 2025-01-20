7f998f04c7f7a769427b9942617128e608b4982b
GinHar <gihape20@gmail.com>
2025-01-20 11:02:18 +0100

    Update solenoid.cpp

--- solenoid.cpp
+++ solenoid.cpp
@@ -50,35 +50,54 @@ double Bz_Integral(long double R,long double t, matrix<long double> r2){
 int main(){
 
   //Data
-  long double R=0.0079;//0.0079m
-  long double I1=0.125;//0.125A
+  long double R=0.005;//0.0079m
+  long double I1=1;//0.125A
   long double mu=4*M_PI*pow(10,-7);
-  long double L=0.704;//0.704m
-  double N=3200;//
+  long double L=0.5;//0.704m
+  double N=320;//
 
   double step=L/N;
-  double L0=-L/2.0;
   
   //The positions of the coils are changed to calculate the effect of a solenoid
 
   matrix<long double> r2(3,1);
-  matrix<long double> B(3,1);
+  matrix<long double> B(3,1); //It is a variable that is going to be changed
+  
+  
+  // Data
+  ofstream outfile("Field_XZ.txt");
 
-  for(int i=0;i<N;i++){
 
-    //We are calculating the magnetic field in the (0,0,0) point
-    r2(0,0)=0;
-    r2(1,0)=0;
-    r2(2,0)=L0+i*step;
+  //We calculated the magnetic field in the XZ plane
 
-    B(0,0)=B(0,0)+Int_Gauss(0,1,R,r2,Bx_Integral);
-    B(1,0)=B(1,0)+Int_Gauss(0,1,R,r2,By_Integral);
-    B(2,0)=B(2,0)+Int_Gauss(0,1,R,r2,Bz_Integral);
+  double l=1; // 1m² is the region that we are going to see
+  double space=0.02; // The distance between each dot is 0.1m
+  double n=l/space; // The number of dots in each axis
+  
+  for(int i=0;i<n;i++){
     
+    for(int j=0;j<n;j++){
+      
+      for(int k=0;k<N;k++){
+
+	r2(0,0)=i*space-l/2.0;
+	r2(2,0)=k*step-L/2.0+j*space-l/2.0;
+
+	B(0,0)=B(0,0)+Int_Gauss(0,1,R,r2,Bx_Integral);
+	B(2,0)=B(2,0)+Int_Gauss(0,1,R,r2,Bz_Integral);
+
+      }
+      B=mu*I1/4/M_PI*B;
+      outfile <<r2(0,0)<<" "<<j*space-l/2.0<<" "<<B(0,0)<<" "<<B(2,0)<<endl;
+      B(0,0)=0;
+      B(2,0)=0;
+              
+    }
   }
 
-  B=mu*I1/4/M_PI*B;
+  outfile.close();
 
-  cout<<B<<endl;
+  cout<<"Magnetic field calculated"<<endl;
   
 }
+
